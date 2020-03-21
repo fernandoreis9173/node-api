1b9d61433d60481178f947d81e99ee2320283972
fernando reis <fernandopw_15cellfaire@hotmail.com>
2020-03-20 21:13:46 -0400

    Connecting Database

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
+// iniciando o app
 const app = express();
 
-app.get ("/", (req, res)=>{
-    res.send("Ola fernando");
-});
+//Iniciando DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+    {
+      useUnifiedTopology: true, 
+      useNewUrlParser: true,     
+      useCreateIndex: true 
+    }
+  );
 
-app.listen(3001);
+ requireDir("./src/models");   
+
+// primeira rota
+app.use("/api",require("./src/routes"));
+
+app.listen(27017);
