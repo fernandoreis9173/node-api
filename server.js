67fe1364c8b1594f119df53d33fbf4bf806f7b5f
fernando reis <fernandopw_15cellfaire@hotmail.com>
2020-03-27 21:17:24 -0400

    update

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 // iniciando o app
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 //Iniciando DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', 
